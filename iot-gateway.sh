2fccf992600871cb86aadfb7286751a98adecb88
Alan Bennett <alan.bennett@linaro.org>
2017-06-08 12:52:34 -0600

    add freeboard dashboard and some helper scripts iot-gateway:  - add tags and freeboard dashboard  - add helper script iot-gateway.sh mosquitto: add default port and another for anonymous websocket accesst hawkbit: add hawkbit and logic to make sure tinyproxy and hawkbit don't both run
    Signed-off-by: Alan Bennett <alan.bennett@linaro.org>

---
+++ iot-gateway.sh
@@ -0,0 +1,24 @@
+# Gateway target hostname
+hostname=[REPLACE WITH YOUR GATEWAY IP]
+sudopass=[GATEWAY SUDO PASSWORD]
+
+# Docker tag to use
+tag=latest-arm64
+
+# Location where hawkbit is running
+gitci=[IP WHERE HAWKBIT IS RUNNING]
+
+#Cloudmqtt configuration
+cloudmqtthost=m12.cloudmqtt.com
+cloudmqttport=18645
+cloudmqttuser=[cloudmqtt username]
+cloudmqttpw=[cloudmqtt password]
+
+#First argument Ansible tags
+ansibletags="$1"
+
+ansible-playbook -e "mqttuser=$cloudmqttuser mqttpass=$cloudmqttpw mqtthost=$cloudmqtthost mqttport=$cloudmqttport "\
+                 -e "gitci=$gitci tag=$tag" \
+                 -e "brokerhost=$hostname brokeruser='' brokerpw=''" \
+                 -e "ansible_sudo_pass=$sudopass" \
+                  -i linaro@$hostname, iot-gateway.yml --tags $ansibletags 
